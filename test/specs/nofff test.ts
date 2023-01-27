a2b4761e25fb3c2989b2e119f92705f16e1e6cc6
Nas Atanasov <atanas.atansov@ignitevision.bg>
2023-01-27 15:21:25 +0200

    update noff test

--- test/specs/nofff test.ts
+++ test/specs/nofff test.ts
@@ -1,14 +1,16 @@
+import loginPage from '../pageobjects/login.page.js';
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
+        await loginPage.open|();
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        //await expect(SecurePage.flashAlert).toHaveTextContaining(
+          //  'You logged into a secure area!');
     });
 });
 
