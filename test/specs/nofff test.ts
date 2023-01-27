bdd83d3e91ea2d5aa0ec12c99b84a1b4bd838a18
Nas Atanasov <atanas.atansov@ignitevision.bg>
2023-01-27 15:30:10 +0200

    git ignor and test changes

--- test/specs/nofff test.ts
+++ test/specs/nofff test.ts
@@ -4,15 +4,14 @@ import SecurePage from '../pageobjects/secure.page.js';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
-        await loginPage.open|();
+        await LoginPage.open();        
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
-        //await expect(SecurePage.flashAlert).toHaveTextContaining(
-          //  'You logged into a secure area!');
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+           'You logged into a secure area!');
     });
-    await mekiq();
+    
 });
 
 
